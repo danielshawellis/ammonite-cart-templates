7bb27607bac29eb8e7efc382b6ace3c39f6848b8
Daniel Ellis <danielshawellis@gmail.com>
2020-02-02 16:08:41 -0600

    add headers and plugin meta

---
+++ ammonite-cart-templates.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name:       Ammonite Cart Templates
+ * Description:       Custom WooCommerce cart template theme overrides
+ * Version:           1.0.0
+ * Author:            Daniel Ellis
+ * Author URI:        https://danielellisdevelopment.com/
+ */
