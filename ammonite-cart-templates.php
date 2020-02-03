13c95e7c40f10de3a8a7f31e6f7e2295861a2245
Daniel Ellis <danielshawellis@gmail.com>
2020-02-03 14:57:41 -0600

    enqueue plugin styles

--- ammonite-cart-templates.php
+++ ammonite-cart-templates.php
@@ -19,8 +19,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 */
 if ( !class_exists( 'Ammonite_Cart_Templates' ) ) {
   class Ammonite_Cart_Templates {
-
+    public static function enqueue_styles() {
+      wp_enqueue_style( 'ammonite-responsive-wc-cart', plugins_url('assets/css/ammonite-responsive-wc-cart.css', __FILE__ ), array(), '1.0.0', 'all' );
+    }
   }
 
   // The below functions run on plugin load
+  add_action( 'wp_enqueue_scripts', array( 'Ammonite_Cart_Templates', 'enqueue_styles' ) );
 }
